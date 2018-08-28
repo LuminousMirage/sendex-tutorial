d8a27f85d8fd924fc3ab9629ce62fc7617ede23e
LuminousMirage <yamateh234@gmail.com>
2018-08-28 21:43:01 +0800

    Updated 17 18

--- 1 basic/python 3 tutorial 16 file writing.py
+++ 1 basic/python 3 tutorial 16 file writing.py
@@ -1,4 +1,4 @@
-text = 'Sample text to Save\nNew line"
+text = 'Sample text to Save\nNew line'
 ##open a file, if it doesn't exist, create one instead. w stands for write to a file, everything in the file will be clear
 ##save to a path two directory infront - '../../exampleFile.txt'
 ##save to c - 'c:/exampleFile.txt'
---
+++ 1 basic/python 3 tutorial 18 file reading.py
@@ -0,0 +1,6 @@
+##read the first line in the exampleFile.txt
+readMe1 = open('exampleFile.txt','r').readline()
+print(readMe1)
+##read all the lines in the exampleFile.txt
+readMe2 = open('exampleFile.txt','r').read()
+print(readMe2)
