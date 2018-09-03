c2143bd88bff1d859bf59e6fb4ca86af90b47d0c
LuminousMirage <yamateh234@gmail.com>
2018-09-03 17:34:00 +0800

    Updated 24

---
+++ 1 basic/python 3 tutorial 24 making modules/examplemod.py
@@ -0,0 +1,6 @@
+##define a module to determine odd or even
+def oddoreven(data):
+	if data % 2 == 0:
+		print('This is an even number.')
+	else:
+		print('This is an odd number.')
---
+++ 1 basic/python 3 tutorial 24 making modules/script.py
@@ -0,0 +1,6 @@
+##import the examplemod module
+import examplemod
+##user input
+x = int(input('Enter a number: '))
+##user result
+examplemod.oddoreven(x)
