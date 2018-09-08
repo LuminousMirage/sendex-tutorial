b2565831e1f20a7745910776203b3ba22035ebcf
LuminousMirage <yamateh234@gmail.com>
2018-09-08 16:09:57 +0800

    Updated 29

--- 1 basic/python 3 tutorial 29 error handling.py
+++ 1 basic/python 3 tutorial 29 error handling.py
@@ -1,30 +1,36 @@
 ##tutorial 28 reading csv.py will be using for the example of error handling
 import csv
-##open the file and load data from exampleData.csv
+
 with open('exampleData.csv') as csvfile:
-##assign the data into readCSV with delimiter seperate with ,
+
 	readCSV = csv.reader(csvfile, delimiter = ',')
 	print(readCSV)
-##declare empty string for dates and colors
+
 	dates = []
 	colors = []
-##loop every row
+
 	for row in readCSV:
 		print('\n', row)
 		print(row[0])
 		print(row[0],row[1])
-##assign color and date
+
 		date = row[0]
 		color = row[3]
-##adding the data into the empty string
+
 		dates.append(date)
 		colors.append(color)
 
 	print('\n', dates)
 	print(colors)
-##let user to find out what date is it when they type in color
-	whatColor = input('What color do you wish the date for?:')
-	colordex = colors.index(whatColor)
-##load the corresponding date with the search of the color
-	theDate = dates[colordex]
-	print('The date of',whatColor,'is:',theDate)
+##try this block of code, handle the errors
+	try:
+		whatColor = input('What color do you wish the date for?:')
+		colordex = colors.index(whatColor)
+		theDate = dates[colordex]
+		print('The date of',whatColor,'is:',theDate)
+##in python 2.7 will use the following instead
+##except Exception, e:
+##whatever the exception to the variable of e
+	except Exception as e:
+		print(e)
+	print('continuing')
