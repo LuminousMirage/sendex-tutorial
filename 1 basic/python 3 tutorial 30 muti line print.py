3117f00d4cda499a5f3a9c1a775274947d7b06d5
LuminousMirage <yamateh234@gmail.com>
2018-09-13 14:17:00 +0800

    Updated 30

---
+++ 1 basic/python 3 tutorial 30 muti line print.py
@@ -0,0 +1,26 @@
+##muti-line comment examples
+print(
+'''
+Testing 1
+Testing 2
+Testing 3
+Testing 1 2 3
+'''
+    )
+
+print('''
+
+It works like a multi-line comment,
+but it will print out.
+
+You can make cool designs like this:
+
+=================
+|               |
+|               |
+|      BOX      |
+|               |
+|               |
+=================
+
+''')
