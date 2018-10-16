31a823fcf215897cea18dc5734d524a141c5a2db
LuminousMirage <yamateh234@gmail.com>
2018-10-16 16:07:55 +0800

    Updated 38

---
+++ 1 basic/python 3 tutorial 38 tkinter basic window.py
@@ -0,0 +1,16 @@
+##import everting from tkinter
+from tkinter import *
+##create a window and makeing a frame
+class Window(Frame):
+##this is the parent wiget our main window
+	def __init__(self, master = None):
+##referencing the initial of the frame class in the library
+		Frame.__init__(self, master)
+##calling the master widget in main window
+		self.master = master
+##root window
+root = Tk()
+##application within that the frame is rooted
+app = Window(root)
+##initialize and generate the window
+root.mainloop()
