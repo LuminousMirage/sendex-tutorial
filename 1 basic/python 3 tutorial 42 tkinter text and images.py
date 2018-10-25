cd73dfdc36cb8c6d0c1107c7804777f391b2bec1
LuminousMirage <yamateh234@gmail.com>
2018-10-25 22:29:27 +0800

    Updated 42

--- 1 basic/python 3 tutorial 42 tkinter text and images.py
+++ 1 basic/python 3 tutorial 42 tkinter text and images.py
@@ -12,24 +12,16 @@ class Window(Frame):
 		self.pack(fill = BOTH, expand = 1)
 ##		quitButton = Button(self, text = "Quit", command = self.client_exit)
 ##		quitButton.place(x = 0, y = 0)
-		
-##creating a menu instance, menu of the main window
+
 		menu = Menu(self.master)
-##define the instance of the menu
 		self.master.config(menu = menu)
 		
-##create the file object
-		file = Menu(menu)
-##adding command in the menu option, call it exit. Also, the command runs on event is client_exit		
+		file = Menu(menu)		
 		file.add_command(label = 'Exit', command = self.client_exit)
-##adding 'file' to the menu		
 		menu.add_cascade(label = 'File', menu = file)
 		
-##create the edit object
 		edit = Menu(menu)
-##adding command in the menu option, call it undo.
 		edit.add_command(label = 'Undo')
-##adding 'edit' to the menu	
 		menu.add_cascade(label = 'Edit', menu = edit)
 		
 	def client_exit(self):
